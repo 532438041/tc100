cf0f0cffbf2d46db55f1b24c2502e4085f1720f7
liuli <liuli@sixdii.com>
2017-02-16 15:56:06 +0800

    后台用户列表功能

--- tc100/src/main/webapp/static/js/system/userList.js
+++ tc100/src/main/webapp/static/js/system/userList.js
@@ -7,5 +7,34 @@
  * @version V1.0
  */
 
-angular.module("indexApp").controller("userListController", function($scope){
+angular.module("indexApp").controller("userListController", function($scope, $http){
+	$scope.pageNum = 1;
+	$scope.pageSize = 10;
+	$scope.total = 0;
+	$scope.showList = function(isCallback) {
+		var param = $(".search-from").serializePageJson($scope.pageNum, $scope.pageSize);
+		$http({
+			method : "post",
+			data : param,
+			url : "/tc100/getUserList.json"
+		}).success(function(dataResult) {
+			$scope.userList = dataResult.data.dataList || [];
+			$scope.total = dataResult.data.total;
+			if (isCallback)
+				return false;
+			$('.page-pager').initPagination({
+				totalCount : $scope.total,
+				limit : $scope.pageSize,
+				callback : function(curr, limit, totalCount) {
+					$scope.pageNum = curr;
+					$scope.showList(true);
+				}
+			});
+		});
+	};
+	$scope.showList();
+
+	$scope.resetSearch = function() {
+		$scope.userName = "";
+	};
 });
