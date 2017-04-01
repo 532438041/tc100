23c1d851128f22890b9c4acdb6a823ec60b853a5
liuli <liuli@sixdii.com>
2017-04-01 15:24:46 +0800

    我的消息-我的推广列表修改

---
+++ tc100/src/main/java/com/java/dto/UserMsgDto.java
@@ -0,0 +1,19 @@
+package com.java.dto;
+
+import com.java.entity.UserMsg;
+
+public class UserMsgDto extends UserMsg {
+
+	private String actType;
+
+	public String getActType() {
+		return actType;
+	}
+
+	public void setActType(String actType) {
+		this.actType = actType;
+	}
+
+	
+
+}
