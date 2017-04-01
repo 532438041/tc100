20dcd7afeface1be4af31634355df2b623b6d4ad
liuli <liuli@sixdii.com>
2017-04-01 16:09:30 +0800

    后台系统参数添加 帖子奖惩添加数据

---
+++ tc100/src/main/resources/sql/tc_sys_param.sql
@@ -0,0 +1,5 @@
+
+insert  into `tc_sys_param`(`id`,`p_name`,`p_key`,`p_value`) values 
+('0d149c70532946719280ea9087b6bfa8','帖子奖惩--分享最大限制','tc_operate_fee_2','10'),
+('9aa155c9eca04ec5b830b31e549d42f9','帖子奖惩--重复最大限制','tc_operate_fee_3','20'),
+('e2389a2063c941138ca1341373c89b2e','帖子奖惩-水贴最大限制','tc_operate_fee_4','20')
